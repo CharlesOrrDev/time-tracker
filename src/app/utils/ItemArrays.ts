66ba7c755d289d2c1d49a52cd5a6384b9daf47c0
HalfwitGoldfish <chas2687@gmail.com>
2025-04-17 19:09:38 -0700

    Partially added item arrays, need to figure out which are missing and complete the empty ones

---
+++ src/app/utils/ItemArrays.ts
@@ -0,0 +1,296 @@
+export const Consumable =
+{
+    HeroesWater:
+    {
+        name: "Heroes Water",
+        location: "Village: Jejunes",
+        time: "9:10 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    SuperSerum:
+    {
+        name: "Super Serum",
+        location: "Village: Jejunes",
+        time: "3:15 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    PlateOfCurry:
+    {
+        name: "Plate of Curry",
+        location: "Village: Jejunes",
+        time: "12:25 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    RiceCake:
+    {
+        name: "Rice Cake",
+        location: "Village: Jejunes",
+        time: "1:20 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    GingerbreadMan:
+    {
+        name: "Gingerbread Man",
+        location: "Village: Dunes",
+        time: "6:25 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    }
+}
+
+export const Throwable =
+{
+    DemonToss:
+    {
+        name: "Demon Toss",
+        location: "Village: Haze",
+        time: "11:40 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    ChiRodToss:
+    {
+        name: "Chi-Rod Toss",
+        location: "Village: Blaze",
+        time: "10:25 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    BatTossHalloween:
+    {
+        name: "Bat Toss Halloween",
+        location: "Village: Blaze",
+        time: "12:20 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    IceShuriken:
+    {
+        name: "Ice Shuriken",
+        location: "Village: Haze",
+        time: "7:05 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    }
+}
+
+export const Dagger =
+{
+    SL2SenkoKunai:
+    {
+        name: "SL2 Senko Kunai",
+        location: "Village: Ember",
+        time: "12:15 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    }
+}
+
+export const Tanto =
+{
+    SaberuTanto:
+    {
+        name: "Saberu Tanto",
+        location: "Training Field",
+        time: "11:20 AM/PM",
+        chance: "1/3 ~ 33.33%"
+    },
+    ShiverTanto:
+    {
+        name: "Shiver Tanto",
+        location: "Forest of Embers",
+        time: "2:45 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    RykanBlade:
+    {
+        name: "Rykan Blade",
+        location: "Village: Haze",
+        time: "2:10 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    KokotsuBlade:
+    {
+        name: "Kokotsu Blade",
+        location: "Village: Obelisk",
+        time: "1:10 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    PikaBlade:
+    {
+        name: "Pika Blade",
+        location: "Village: Tempest",
+        time: "5:32 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    GrassTanto:
+    {
+        name: "Grass Tanto",
+        location: "Dawn Hideout",
+        time: "8:20 AM/PM",
+        chance: "1/4 ~ 25.00%"
+    },
+    AcrobatStyle:
+    {
+        name: "Acrobat Style",
+        location: "Village: Nimbus",
+        time: "6:15 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    }
+}
+
+export const Katana =
+{
+    ShizenRaijin:
+    {
+        name: "Shizen Raijin",
+        location: "Village: Ember",
+        time: "3:15 AM/PM",
+        chance: "1/3 ~ 33.33%"
+    },
+    ShindoBlade:
+    {
+        name: "Shindo Blade",
+        location: "Village: Nimbus",
+        time: "4:55 AM/PM",
+        chance: "1/3 ~ 33.33%"
+    },
+    RaionBlade:
+    {
+        name: "Raion Blade",
+        location: "Village: Obelisk",
+        time: "2:25 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    SamuraiTanto:
+    {
+        name: "Samurai Tanto",
+        location: "Village: Nimbus",
+        time: "4:45 AM/PM",
+        chance: "1/4 ~ 25.00%"
+    },
+    SatoriBlade:
+    {
+        name: "Satori Blade",
+        location: "Village: Ember",
+        time: "2:10 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    ElectroBlade:
+    {
+        name: "Electro Blade",
+        location: "Village: Ember",
+        time: "2:50 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    DioSenkoBlade:
+    {
+        name: "Dio Senko Blade",
+        location: "Village: Ember",
+        time: "12:35 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    FireworkKatana:
+    {
+        name: "Firework Katana",
+        location: "Village: Ember",
+        time: "1:45 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    ObeliskChiBlade:
+    {
+        name: "Obelisk Chi Blade",
+        location: "Village: Obelisk",
+        time: "8:25 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    NimbusChiBlade:
+    {
+        name: "Nimbus Chi Blade",
+        location: "Village: Nimbus",
+        time: "11:25 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    EmberChiBlade:
+    {
+        name: "Ember Chi Blade",
+        location: "Village: Ember",
+        time: "9:25 AM/PM",
+        chance: "1/5 ~ 20.00%"
+    },
+    DunesChiBlade:
+    {
+        name: "Dunes Chi Blade",
+        location: "Village: Dunes",
+        time: "10:25 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    ApolloBlade:
+    {
+        name: "Apollo Blade",
+        location: "Great Narumaki Bridge",
+        time: "10:45 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    SL2GrassTanto:
+    {
+        name: "SL2 Grass Tanto",
+        location: "Village: Obelisk",
+        time: "4:15 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    }
+}
+
+export const Heavy =
+{
+
+}
+
+export const Spear =
+{
+    TripleCobaltBlade:
+    {
+        name: "Triple Cobalt Blade",
+        location: "Village: Obelisk",
+        time: "7:25 AM/PM",
+        chance: "1/4 ~ 25.00%"
+    },
+    ThreadBlade:
+    {
+        name: "Thread Blade",
+        location: "Village: Haze",
+        time: "6:30 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    CandyCaneBlade:
+    {
+        name: "Candy Cane Blade",
+        location: "Village: Haze",
+        time: "2:15 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    },
+    SL2ThreadBlade:
+    {
+        name: "SL2 Thread Blade",
+        location: "Village: Haze",
+        time: "3:15 AM/PM",
+        chance: "1/7 ~ 14.29%"
+    }
+}
+
+export const Dual =
+{
+
+}
+
+export const Instrument =
+{
+
+}
+
+export const Items =
+{
+    Consumable: Consumable,
+    Throwable: Throwable,
+    Dagger: Dagger,
+    Tanto: Tanto,
+    Katana: Katana,
+    Heavy: Heavy,
+    Spear: Spear,
+    Dual: Dual,
+    Instrument: Instrument
+}
